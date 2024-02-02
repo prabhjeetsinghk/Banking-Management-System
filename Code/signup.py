9f4e0a92676e9d70d645eb498f547c2872b70fbb
Prabhjeet Singh <prabhjeetsingh.ai@gmail.com>
2024-02-01 21:55:42 -0500

    intergation of streamlit

--- Code/signup.py
+++ Code/signup.py
@@ -2,12 +2,31 @@ from dbConnection1 import UserDatabase
 from encdecString import encdec
 from datetime import datetime
 
-
+import streamlit as st
 class user_signup:
+
+    def render(self):
+        st.title("User Registeration Page")
+
+        # Input Name
+        user_name = st.text_input("Enter your name", "")
+
+        # Input Email
+        user_email = st.text_input("Enter your email", "")
+
+        # Input Password
+        user_email = st.text_input("Enter your password", "", type="password")
+
+        # # Number Input
+        # age = st.number_input("Enter your age", 0, 100, 25)
+
+        # Date Input
+        birth_date = st.date_input("Select your birth date")
+
     @staticmethod
     def add_user_details():
         db = UserDatabase()
-
+        st.title("Streamlit App")
         user_name = input("Enter your full name:\n")
         user_email = input("Enter your email:\n")
         user_pwd = input("Enter your password:\n")
@@ -27,7 +46,7 @@ class user_signup:
         if result.rowcount:
             print('Record Added Successfully')
             balance = 0.0
-            
+
             add_balance_sql_query = "INSERT INTO account_balance (user_id, balance) VALUES (?,?)"
             params = (user_email,balance)
 
