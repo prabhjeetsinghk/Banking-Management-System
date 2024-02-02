1546b6ba3fca8a0ec606fa1ffc2b9f19f3fc5e7e
Prabhjeet Singh <148787330+prabhjeetsinghk@users.noreply.github.com>
2024-02-02 10:25:27 +0000

    html pages

--- Code/app.py
+++ Code/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
